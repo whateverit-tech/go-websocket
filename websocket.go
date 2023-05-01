9fe5989ee8f6fd1405dfe54b2d16917b6f1b263d
Rahmat Kurniawan <rahmatkurniawan095@gmail.com>
2023-05-01 16:50:01 +0700

    update websocket

--- websocket.go
+++ websocket.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/websocket"
@@ -10,32 +11,74 @@ import (
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+type ChatMessage struct {
+	Username string `json:"username"`
+	Text     string `json:"text"`
+}
+
+var clients = make(map[*websocket.Conn]bool)
+var broadcaster = make(chan ChatMessage)
+
+func handleConnection(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer ws.Close()
+	clients[ws] = true
+
+	for {
+		var msg ChatMessage
+		// Read in a new message as JSON and map it to a Message object
+
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			delete(clients, ws)
+			break
+		}
+		// send new message to the channel
+		broadcaster <- msg
+	}
+}
+
+func handleMessages() {
+	for {
+		// grab any next message from channel
+		msg := <-broadcaster
+		messageClients(msg)
+	}
+}
+
+func unsafeError(err error) bool {
+	return !websocket.IsCloseError(err, websocket.CloseGoingAway) && err != io.EOF
+}
+
+func messageClients(msg ChatMessage) {
+	// send to every client currently connected
+	for client := range clients {
+		messageClient(client, msg)
+	}
+}
+
+func messageClient(client *websocket.Conn, msg ChatMessage) {
+	err := client.WriteJSON(msg)
+	log.Println("message ", msg)
+	if err != nil && unsafeError(err) {
+		log.Printf("error: %v", err)
+		client.Close()
+		delete(clients, client)
+	}
 }
 
 func main() {
-	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
-		conn, _ := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
-
-		for {
-			// Read message from browser
-			msgType, msg, err := conn.ReadMessage()
-			if err != nil {
-				return
-			}
-
-			// Print the message to the console
-			fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
-
-			// Write message back to browser
-			if err = conn.WriteMessage(msgType, msg); err != nil {
-				return
-			}
-		}
-	})
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "websockets.html")
-	})
-
+	http.HandleFunc("/websocket", handleConnection)
+	go handleMessages()
 	http.ListenAndServe(":8080", nil)
 }
